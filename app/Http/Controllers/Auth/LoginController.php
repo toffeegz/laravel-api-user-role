d3b65abb343e597ec08f42c6034abb19a34dad5b
gezrylclariz <gezrylclarizg@gmail.com>
2022-06-27 16:01:34 +0800

    Update LoginController.php
    return 401 with error message

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,8 +9,18 @@ class LoginController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        
         if(!$token = auth()->attempt($request->only('email', 'password'))) {
-            return response(null, 401);
+            // return response(null, 401);
+            // return response(null, 401);
+            $data = [
+                ['Your account or password is incorrect']
+            ];
+            return response()->json(['errors'=>$data], 401);
         }
 
         return response()->json(compact('token'));
